f0797ab2314e90f9bf6d32888097271df32db4fe
readlnh <readlnh@163.com>
2024-05-08 04:55:33 +0200

    fix sys_get_time and sys_task_info

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -4,11 +4,12 @@ use alloc::sync::Arc;
 use crate::{
     config::MAX_SYSCALL_NUM,
     loader::get_app_data_by_name,
-    mm::{translated_refmut, translated_str},
+    mm::{translated_refmut, translated_str, translated_byte_buffer},
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, TaskStatus,
     },
+    timer::get_time_us,
 };
 
 #[repr(C)]
@@ -117,23 +118,65 @@ pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
+    if ts.is_null() {
+        return -1;
+    }
+
+    const SIZE: usize = core::mem::size_of::<TimeVal>();
+    let translated_ts = translated_byte_buffer(current_user_token(), ts as *const u8, SIZE);
+    if translated_ts.is_empty() {
+        return -1;
+    }
+    let us = get_time_us();
+    unsafe {
+        if translated_ts[0].len() < SIZE {
+            return -1;
+        }
+        let translated_ts_ptr = translated_ts[0].as_ptr() as *mut TimeVal;
+        *translated_ts_ptr = TimeVal {
+            sec: us / 1_000_000,
+            usec: us % 1_000_000,
+        };
+    }
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TaskInfo`] is splitted by two pages ?
 pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_task_info NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
-    -1
+    trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
+
+    // if ti.is_null() {
+    //     return -1;
+    // }
+    // let mut translated_ti = translated_byte_buffer(
+    //     current_user_token(),
+    //     ti as *const u8,
+    //     core::mem::size_of::<TaskInfo>(),
+    // );
+
+    // // Check if the translated buffer is empty
+    // if translated_ti.is_empty() {
+    //     return -1;
+    // }
+
+    // // Ensure that the translated buffer has enough bytes to store a `TaskInfo`
+    // if translated_ti[0].len() < core::mem::size_of::<TaskInfo>() {
+    //     return -1;
+    // }
+
+    // Fill in the task information
+    // unsafe {
+    //     let ti_ptr = translated_ti[0].as_mut_ptr() as *mut TaskInfo;
+    //     (*ti_ptr).status = get_task_status();
+    //     (*ti_ptr).syscall_times = get_syscall_times();
+    //     (*ti_ptr).time = get_times();
+    // }
+
+    0
 }
 
 /// YOUR JOB: Implement mmap.
